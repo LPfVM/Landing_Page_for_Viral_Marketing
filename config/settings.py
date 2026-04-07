a191d8ebeef009674774a55a4c96f8337d2cc0b0
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-04-07 17:28:36 +0900

    feat:user models.py 및 serializers.py 생성

--- config/settings.py
+++ config/settings.py
@@ -127,3 +127,15 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
 }
+
+# User
+AUTH_USER_MODEL = "user.User"
+
+# Email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = "smtp.naver.com"
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_PORT = 465
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
