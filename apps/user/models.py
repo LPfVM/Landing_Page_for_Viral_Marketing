9018f4aa41c8d0023ef20065b1fb7e3bf8e3b83a
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-04-07 18:37:38 +0900

    user model is_staff 추가 및 ruff 진행

--- apps/user/models.py
+++ apps/user/models.py
@@ -2,10 +2,11 @@ from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 
+
 class UserManager(BaseUserManager):
     def create_user(self,email,password,nickname,**kwargs):
         if not email:
-            raise ValueError
+            raise ValueError("이메일은 필수입니다.")
         user = self.model(
             email=self.normalize_email(email),
             nickname = nickname,
@@ -19,6 +20,7 @@ class UserManager(BaseUserManager):
         user = self.create_user(email,password,nickname,**kwargs)
         user.is_admin = True
         user.is_superuser = True
+        user.is_staff = True
         user.save(using = self._db)
         return user
 
@@ -28,12 +30,19 @@ class User(AbstractBaseUser):
     is_admin = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
+    is_staff =models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     objects = UserManager()
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['nickname']
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["nickname"]
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return self.is_admin
 
     class Meta:
         verbose_name = "Users"
