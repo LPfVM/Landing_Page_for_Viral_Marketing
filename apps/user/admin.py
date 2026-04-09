eaa416faa9a6df5a2ba6bdd703d7475de2dcd05a
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-04-09 10:12:21 +0900

    feat: user admin 추가

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -7,3 +7,6 @@ from .models import User
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ["email", "nickname", "is_active", "is_admin", "created_at"]
+    search_fields = ["email", "nickname"]
+    list_filter = ["is_active", "is_staff"]
+    readonly_fields = ["is_staff", "is_admin", "is_superuser"]
