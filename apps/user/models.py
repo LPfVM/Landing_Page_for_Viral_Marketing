a76d815dc49209f703bff3305b3697cc85968adf
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-04-09 10:27:15 +0900

    Feature/user/view (#9)
    * feat:user_view 구현
    
    * refactor:user view service 분리
    
    * feat:jwt 토큰 추가 및 로그아웃기능 추가
    
    * fix:service 변수명,permission_classe
    
    * feat: user admin 추가
    
    * 충돌 해결

--- apps/user/models.py
+++ apps/user/models.py
@@ -4,33 +4,34 @@ from django.db import models
 
 
 class UserManager(BaseUserManager):
-    def create_user(self,email,password,nickname,**kwargs):
+    def create_user(self, email, password, nickname, **kwargs):
         if not email:
             raise ValueError("이메일은 필수입니다.")
         user = self.model(
             email=self.normalize_email(email),
-            nickname = nickname,
+            nickname=nickname,
             **kwargs,
         )
         user.set_password(password)
-        user.save(using = self._db)
+        user.save(using=self._db)
         return user
 
-    def create_superuser(self,email,password,nickname,**kwargs):
-        user = self.create_user(email,password,nickname,**kwargs)
+    def create_superuser(self, email, password, nickname, **kwargs):
+        user = self.create_user(email, password, nickname, **kwargs)
         user.is_admin = True
         user.is_superuser = True
         user.is_staff = True
-        user.save(using = self._db)
+        user.save(using=self._db)
         return user
 
+
 class User(AbstractBaseUser):
-    nickname = models.CharField(max_length=50,unique=True)
+    nickname = models.CharField(max_length=50, unique=True)
     email = models.EmailField(max_length=255, unique=True)
     is_admin = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
-    is_staff =models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,1 +1,136 @@
-# Create your views here.
+from django.contrib.auth import get_user_model
+from django.core.signing import (
+    BadSignature,
+    SignatureExpired,
+)
+from rest_framework import status
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.exceptions import TokenError
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from .serializers import (
+    UserLoginSerializer,
+    UserProfileSerializer,
+    UserSignUPSerializer,
+)
+from .service import (
+    activate_email_user,
+    deactivate_user,
+    send_verification_email,
+    verify_email_code,
+)
+
+User = get_user_model()
+
+
+class UserSignUpView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = UserSignUPSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            send_verification_email(user, request)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            {"message": "email 인증 링크를 발송했습니다."},
+            status=status.HTTP_201_CREATED,
+        )
+
+
+class EmailVerifyView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        code = request.GET.get("code", None)
+        try:
+            email = verify_email_code(code)
+        except (TypeError, SignatureExpired, BadSignature):
+            return Response(
+                {"message": "유효하지 않은 인증 링크입니다."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        activate_email_user(email)
+
+        return Response(
+            {"message": "회원가입이 성공적으로 완료되었습니다."},
+            status=status.HTTP_200_OK,
+        )
+
+
+class UserLoginView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = UserLoginSerializer(
+            data=request.data, context={"request": request}
+        )
+        if serializer.is_valid():
+            user = serializer.validated_data["user"]
+            refresh = RefreshToken.for_user(user)
+            return Response(
+                {
+                    "refresh": str(refresh),
+                    "access": str(refresh.access_token),
+                }
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+
+class UserProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self, pk):
+        user = get_object_or_404(User, pk=pk)
+        if self.request.user != user:
+            raise PermissionDenied("권환 없음")
+        return user
+
+    def get(self, request, pk):
+        user = self.get_object(pk=pk)
+        serializer = UserProfileSerializer(user)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        user = self.get_object(pk=pk)
+        serializer = UserProfileSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        user = self.get_object(pk=pk)
+        deactivate_user(user)
+
+        return Response(
+            {"message": "Deleted successfully"},
+            status=status.HTTP_204_NO_CONTENT,
+        )
+
+
+class UserLogoutView(APIView):
+    def post(self, request):
+        refresh_token = request.data.get("refresh")
+        try:
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+        except TokenError:
+            return Response(
+                {"message": "유효하지 않은 토큰 입니다."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        return Response(
+            {
+                "message": "로그아웃 되었습니다.",
+            },
+            status=status.HTTP_200_OK,
+        )
