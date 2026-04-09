b4cbf7c81a95d3c75c3c70ff661e3119d31d02b5
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-04-09 16:36:15 +0900

    fix: analysis 생성 url create 추가

--- apps/analysis/urls.py
+++ apps/analysis/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import AnalysisListView, AnalysisView
 
 urlpatterns = [
-    path("analysis/", AnalysisView.as_view(), name="analysis_create"),
+    path("analysis/create/", AnalysisView.as_view(), name="analysis_create"),
     path("analysis/<int:pk>/", AnalysisView.as_view(), name="analysis_detail"),
     path("analysis/list/", AnalysisListView.as_view(), name="analysis_list"),
 ]
