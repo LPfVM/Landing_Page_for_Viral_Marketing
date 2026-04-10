b69c768cf6da097648b7d3a60037d2719f807bce
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-04-10 18:47:17 +0900

    feat: 쿠키값 설정 및 user test 변경 (#25)

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
-    TokenRefreshView,
     TokenVerifyView,
 )
 
@@ -17,7 +16,7 @@ urlpatterns = [
     path("users/logout/", views.UserLogoutView.as_view(), name="user_logout"),
     path("users/<int:pk>/", views.UserProfileView.as_view(), name="user_profile"),
     # jwt
-    path("token", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
-    path("token/verify", TokenVerifyView.as_view(), name="token_verify"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", views.UserRefreshTokenView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
